8dd5f938399090bbc4d8dc1045889ead4305bd10
Zharphyn <brad@zharphyn.ca>
2018-02-20 21:17:25 +0000

    Cleaned up linting

--- download_avatars.js
+++ download_avatars.js
@@ -16,55 +16,55 @@ function getOptions(repoOwner, repoName) {
     
     headers: {
       'User-Agent': 'request',
-      'Authorization' : "token " + token 
+      'Authorization': "token " + token
     }
   };
-	return options;
+  return options;
 }
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  request(getOptions(repoOwner,repoName), function(err, res, body) {
+  request(getOptions(repoOwner, repoName), function(err, res, body) {
     cb(err, res, body);
-   });
+  });
 }
 
 // takes a passed url, and write contents to a file
 function downloadImageByURL(url, filePath) {
   request.get(url, function(err, res, body){
     if (err){
-    	console.log(err);
-    } 
+      console.log(err);
+    }
     console.log('Downloading...' + url);
   })
-  .pipe(fs.createWriteStream(filePath));
+    .pipe(fs.createWriteStream(filePath));
 
 }
 
 var callback = function(err, res){
-	console.log(res);
+  console.log(res);
 };
 
 function getOwnerAndName(){
   if (process.argv.length >= 4) {
-  	owner = process.argv[2];
+    owner = process.argv[2];
     name = process.argv[3];
   } else {
-	console.log('Please supply the owner and name of the repository.');
-	return false;
+    console.log('Please supply the owner and name of the repository.');
+    return false;
   }
   return true;
 }
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 if (getOwnerAndName()) {
-  getRepoContributors(owner,name, function(err, res, body) {
+  getRepoContributors(owner, name, function(err, res, body) {
     var org = JSON.parse(body);
     for (var item in org){
-   	  var filepath = `./images/${org[item].login}.jpg`;
-   	  downloadImageByURL(org[item].avatar_url,filepath);
-   	  if (err) {
-        console.log (err);
-   	  }
+      var filepath = `./images/${org[item].login}.jpg`;
+      downloadImageByURL(org[item].avatar_url, filepath);
+      if (err) {
+        console.log(err);
+      }
     }
   });
 }
