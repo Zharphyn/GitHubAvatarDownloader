7616bee7d7593fd3899388a6edb85f8e809b9975
Zharphyn <brad@zharphyn.ca>
2018-02-20 16:55:36 +0000

    step 7

--- download_avatars.js
+++ download_avatars.js
@@ -1,12 +1,36 @@
+/*jshint esversion: 6 */
+
 var request = require('request');
+var token = require('./token');
+const username = 'Zharphyn';
+const globalURL = 'https://api.github.com/repos/';
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  // ...
+  var options = {
+    url: globalURL + repoOwner + "/" + repoName + "/contributors",
+    
+    headers: {
+      'User-Agent': 'request',
+      'Authorization' : "token " + token 
+    }
+  };
+
+  //var url = globalURL + repoOwner + "/" + repoName + "/contributors";
+  
+  request(options, function(err, res, body) {
+    cb(err, body);
+    
+  });
 }
 
-getRepoContributors("jquery", "jquery", function(err, result) {
-  console.log("Errors:", err);
-  console.log("Result:", result);
-});
+ getRepoContributors('jquery','jquery', function(err, result) {
+  // console.log("Errors:", err);
+  // console.log("Result:", result);
+   var org = JSON.parse(result);
+   for (var item in org){
+   	console.log(org[item].avatar_url);
+   }
+   
+ });
